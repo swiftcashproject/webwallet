b3b9c4b7022f767f42cd610bcf0bd89bb585a6a2
msg768 <markus.ghambari@gmail.com>
2020-07-18 13:17:45 +1000

    donate

--- js/script.js
+++ js/script.js
@@ -558,7 +558,11 @@ function spendf() {
   // Add the change (if any)
   var change = SWIFT(balance - amount - FEE);
   if (change > 0) {
-    tx.addOutput(changeAddress, Math.ceil(change*100000000));
+    // Try to avoid dusting in the SwiftCash blockchain
+    if (whichCoin == "SWIFT" && change < 1 && confirm("Change is less than 1 SWIFT. Press OK to donate it, or Cancel to keep it."))
+      donation = change;
+    else
+      tx.addOutput(changeAddress, Math.ceil(change*100000000));
   }
 
   // Add the donation output (if any)
